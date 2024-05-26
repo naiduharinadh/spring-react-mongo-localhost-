858a64d82cac8f98ecd0fc51c3794764b25a7637
hari nadh <naiduhari2001@gmail.com>
2024-05-26 23:41:32 +0530

    initial commit

---
+++ project2/src/main/java/com/project2/project2/controllers/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.project2.project2.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Add your frontend URL here
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Add allowed HTTP methods
+                .allowCredentials(true); // Allow cookies
+    }
+}
+
