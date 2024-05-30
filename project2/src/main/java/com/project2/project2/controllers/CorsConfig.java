d04a17e4ffe16e516fe3da0b0d26b0dea539eacd
naiduharinadh <137928332+naiduharinadh@users.noreply.github.com>
2024-05-30 12:38:29 +0530

    cors updated for react with svc

--- project2/src/main/java/com/project2/project2/controllers/CorsConfig.java
+++ project2/src/main/java/com/project2/project2/controllers/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Add your frontend URL here
+                .allowedOrigins("http://a215df3f2d1ab47ba9905ccfaf214f56-156782520.ap-southeast-1.elb.amazonaws.com:80") // Add your frontend URL here
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Add allowed HTTP methods
                 .allowCredentials(true); // Allow cookies
     }
